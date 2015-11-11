9114296b63b3325e73d3d44d09b127b6d28038c9
sergeynikiforov <acanthurus.sn@gmail.com>
2015-11-11 15:48:13 +0300

    add smooth scroll sticky sidebar to resume page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,39 @@
 $(document).foundation();
+
+// smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// smooth sticky sidebar
+(function($) {
+    var element = $('.sticky'),
+        originalY = element.offset().top;
+
+    // Space between element and top of screen (when scrolling)
+    var topMargin = 20;
+
+    // Should probably be set in CSS; but here just for emphasis
+    element.css('position', 'relative');
+
+    $(window).on('scroll', function(event) {
+        var scrollTop = $(window).scrollTop();
+
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 700);
+    });
+})(jQuery);
