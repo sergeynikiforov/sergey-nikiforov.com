6b3e97a2533145acebc91c82e9b669aadafcb978
sergeynikiforov <acanthurus.sn@gmail.com>
2015-11-18 01:10:09 +0300

    update offset menu scss & js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,24 @@
-$(document).foundation();
+$(document).foundation({
+    offcanvas : {
+        // Should the menu close when a menu link is clicked?
+        // [ true | false ]
+        close_on_click : true
+      }
+});
 
 /* smooth scroll
  * snippet taken from
  * https://css-tricks.com/snippets/jquery/smooth-scrolling/
-*/
-
+ */
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
+
+    // if link clicked within off-canvas menu, close the menu
+    if ($(".exit-off-canvas-link").length > 0) {
+        $(".exit-off-canvas-link").on("click.toggleCanvas", function(){
+              $(".exit-off-canvas").click();
+            });
+    }
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -20,32 +32,42 @@ $(function() {
   });
 });
 
+
 /* smooth sticky sidebar
  * taken from
  * http://stackoverflow.com/questions/2177983/how-to-make-div-follow-scrolling-smoothly-with-jquery
-*/
+ */
 $(function() {
-    var element = $('.sticky-sidebar'),
-        originalY = element.offset().top;
 
-    // Space between element and top of screen (when scrolling)
-    var topMargin = 20;
+    var element = $('.sticky-sidebar');
 
-    // Should probably be set in CSS; but here just for emphasis
-    element.css('position', 'relative');
+    // check if element is present
+    if (element.length > 0) {
+        var originalY = element.offset().top;
 
-    $(window).on('scroll', function(event) {
-        var scrollTop = $(window).scrollTop();
+        // Space between element and top of screen (when scrolling)
+        var topMargin = 20;
 
-        element.stop(false, false).animate({
-            top: scrollTop < originalY
-                    ? 0
-                    : scrollTop - originalY + topMargin
-        }, 700);
-    });
+        // Should probably be set in CSS; but here just for emphasis
+        element.css('position', 'relative');
+
+        $(window).on('scroll', function(event) {
+            var scrollTop = $(window).scrollTop();
+
+            element.stop(false, false).animate({
+                top: scrollTop < originalY
+                        ? 0
+                        : scrollTop - originalY + topMargin
+            }, 700);
+        });
+    }
 });
 
-// owl carousel
+
+/*
+ * owl carousel
+ *
+ */
 $(document).ready(function() {
 
     var owl = $("#owl-example");
@@ -70,5 +92,4 @@ $(document).ready(function() {
     $(".prev").click(function() {
       owl.trigger('owl.prev');
     });
-
 });
--- sn_app/static/sn_app/js/app.js
+++ sn_app/static/sn_app/js/app.js
@@ -16488,15 +16488,27 @@ return jQuery;
   };
 }(jQuery, window, window.document));
 
-$(document).foundation();
+$(document).foundation({
+    offcanvas : {
+        // Should the menu close when a menu link is clicked?
+        // [ true | false ]
+        close_on_click : true
+      }
+});
 
 /* smooth scroll
  * snippet taken from
  * https://css-tricks.com/snippets/jquery/smooth-scrolling/
-*/
-
+ */
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
+
+    // if link clicked within off-canvas menu, close the menu
+    if ($(".exit-off-canvas-link").length > 0) {
+        $(".exit-off-canvas-link").on("click.toggleCanvas", function(){
+              $(".exit-off-canvas").click();
+            });
+    }
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -16510,32 +16522,42 @@ $(function() {
   });
 });
 
+
 /* smooth sticky sidebar
  * taken from
  * http://stackoverflow.com/questions/2177983/how-to-make-div-follow-scrolling-smoothly-with-jquery
-*/
+ */
 $(function() {
-    var element = $('.sticky-sidebar'),
-        originalY = element.offset().top;
 
-    // Space between element and top of screen (when scrolling)
-    var topMargin = 20;
+    var element = $('.sticky-sidebar');
 
-    // Should probably be set in CSS; but here just for emphasis
-    element.css('position', 'relative');
+    // check if element is present
+    if (element.length > 0) {
+        var originalY = element.offset().top;
 
-    $(window).on('scroll', function(event) {
-        var scrollTop = $(window).scrollTop();
+        // Space between element and top of screen (when scrolling)
+        var topMargin = 20;
 
-        element.stop(false, false).animate({
-            top: scrollTop < originalY
-                    ? 0
-                    : scrollTop - originalY + topMargin
-        }, 700);
-    });
+        // Should probably be set in CSS; but here just for emphasis
+        element.css('position', 'relative');
+
+        $(window).on('scroll', function(event) {
+            var scrollTop = $(window).scrollTop();
+
+            element.stop(false, false).animate({
+                top: scrollTop < originalY
+                        ? 0
+                        : scrollTop - originalY + topMargin
+            }, 700);
+        });
+    }
 });
 
-// owl carousel
+
+/*
+ * owl carousel
+ *
+ */
 $(document).ready(function() {
 
     var owl = $("#owl-example");
@@ -16560,7 +16582,6 @@ $(document).ready(function() {
     $(".prev").click(function() {
       owl.trigger('owl.prev');
     });
-
 });
 /*!
  * jQuery Sticky Footer 2.1
