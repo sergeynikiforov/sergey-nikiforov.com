a0bde69b711e76f11fb2f79cba6d7f629230f71a
sergeynikiforov <acanthurus.sn@gmail.com>
2015-12-21 16:27:46 +0300

    fix sidebar margin-left behaviour

--- sn_app/static/sn_app/js/app.js
+++ sn_app/static/sn_app/js/app.js
@@ -21003,12 +21003,18 @@ MyWaypoints.turnTopBarFontColorWhite = function () {
 
 
 // sticky sidebar on photoset page
+//
+//
+
 var StickySidebar = {};
 
+// helper switches
 StickySidebar.isSetUp = false;
 StickySidebar.isTopBarSetUp = false;
 StickySidebar.isSidebarFixed = false;
 StickySidebar.isSidebarBottom = false;
+
+// helper functions
 
 StickySidebar.changeTopBarBackgroundColor = function () {
     $('#top-bar-wrapper > div').toggleClass('white-bg');
@@ -21025,54 +21031,54 @@ StickySidebar.removeTopBarBackgroundColor = function () {
     $('#top-bar-wrapper > .fixed').css('height', 'auto');
 }
 
-StickySidebar.setStickySidebarColumnHeight = function () {
-    // sets sidebar wrapper column equal to thumbs height
-    StickySidebar.thumbsHeight = $('#thumbs').outerHeight();
-    $('#fixed-wrapper > div').outerHeight(StickySidebar.thumbsHeight);
-}
 
-StickySidebar.setStickySidebarColumnWidth = function () {
-
-    // initial assingments
-    if (StickySidebar.stickySidebarColumnWidth === undefined) {
-        StickySidebar.sidebarLeftOffset = $('#sidebar-wrapper').offset().left;
-        StickySidebar.stickySidebarColumnWidth = $('#fixed-wrapper > div').outerWidth();
-        $('#fixed-wrapper > div').outerWidth(StickySidebar.stickySidebarColumnWidth);
-    } else {
-        // if sidebar is fixed
-        if ($('.fixed-sidebar').length > 0) {
-            StickySidebar.stickySidebarColumnWidth = parseInt($('#thumbs').css('margin-left'));
-            $('#fixed-wrapper > div').outerWidth(StickySidebar.stickySidebarColumnWidth);
-        } else {
-            // if sidebar is not fixed
-            $('#sidebar-wrapper').css('margin-left', '0px');
-            StickySidebar.stickySidebarColumnWidth = $(window).innerWidth() - $('#thumbs').outerWidth();
-            $('#fixed-wrapper > div').outerWidth(StickySidebar.stickySidebarColumnWidth);
-        };
-    }
-}
-
+// add fixed classes to sidebar
 StickySidebar.makeSidebarFixed = function() {
     $('#fixed-wrapper').addClass('fixed-sidebar');
     $('#thumbs-wrapper > div').addClass('large-offset-4');
     $('#sidebar-wrapper').removeClass('content-down');
     StickySidebar.isSidebarFixed = true;
+    StickySidebar.setStickySidebarColumnMargins();
 }
 
+// remove fixed classes from sidebar
 StickySidebar.makeSidebarUnFixed = function() {
     $('#fixed-wrapper').removeClass('fixed-sidebar');
     $('#thumbs-wrapper > div').removeClass('large-offset-4');
     StickySidebar.isSidebarFixed = false;
+    StickySidebar.setStickySidebarColumnMargins();
 }
 
+// place sidebar on the bottom of its container
 StickySidebar.placeSidebarBottom = function() {
     $('#sidebar-wrapper').addClass('content-down');
     StickySidebar.isSidebarBottom = true;
 }
 
+// lift sidebar from the bottom of its container
 StickySidebar.liftSidebarFromBottom = function() {
     $('#sidebar-wrapper').removeClass('content-down');
     StickySidebar.isSidebarBottom = false;
+}
+
+// sets sidebar wrapper column equal to thumbs height
+StickySidebar.setStickySidebarColumnHeight = function () {
+    StickySidebar.thumbsHeight = $('#thumbs').outerHeight();
+    $('#fixed-wrapper > div').outerHeight(StickySidebar.thumbsHeight);
+}
+
+// sets margin-left of the sidebar, called on ready, resize, make sidebar fixed/unfixed
+StickySidebar.setStickySidebarColumnMargins = function () {
+
+    // initial assingments, align with top-bar left
+    StickySidebar.stickySidebarColumnLeftMargin = $('#header-wrapper').offset().left;
+
+    // if sidebar is fixed
+    if (StickySidebar.isSidebarFixed) {
+        $('#fixed-wrapper > div').attr('style', 'margin-left:' + StickySidebar.stickySidebarColumnLeftMargin + 'px !important;');
+    } else { // if sidebar is not fixed
+        $('#fixed-wrapper > div').css('margin-left', 'auto');
+    };
 }
 
 // function is called whenever we scroll or resize
@@ -21092,6 +21098,7 @@ StickySidebar.stickySidebar = function() {
         if (StickySidebar.topbarBottom < StickySidebar.makeFixedTop) {
             StickySidebar.makeSidebarUnFixed();
         };
+
     } else { // NOT fixed
 
         if (StickySidebar.isSidebarBottom) {
@@ -21139,40 +21146,22 @@ StickySidebar.setUpStickySidebar = function() {
     // set it only on large screens && on page with thumbs & sidebar-wrapper
     if ($('#sidebar-wrapper').length > 0 && $('#thumbs').length > 0 && window.innerWidth > 1024) {
 
-        StickySidebar.sidebarContentHeight = $('#sidebar-wrapper').outerHeight();
+        // set content height, used in stickySidebar()
+        StickySidebar.sidebarContentHeight = $('#sidebar-wrapper > div').outerHeight();
+
+        // call set-up functions
         StickySidebar.setUpTopBar();
         StickySidebar.setStickySidebarColumnHeight();
-        StickySidebar.setStickySidebarColumnWidth();
+        StickySidebar.setStickySidebarColumnMargins();
         StickySidebar.stickySidebar();
 
+        // switch on
         StickySidebar.isSetUp = true;
 
     } else {
         StickySidebar.isSetUp = false;
     };
 }
-
-
-// toggle fixed class
-/*
-StickySidebar.waypointsMakeFixed = new Waypoint({
-    element: document.getElementById('make-fixed'),
-    handler: function(direction) {
-        if (direction == 'down') {
-            if (StickySidebar.topbarBottom < StickySidebar.sidebarBottom) {
-                $('#fixed-wrapper').addClass('fixed-sidebar');
-                $('#thumbs-wrapper > div').addClass('large-offset-4');
-                $('#sidebar-wrapper').removeClass('content-down');
-            };
-            StickySidebar.changeTopBarBackgroundColor();
-        } else {
-            $('#fixed-wrapper').removeClass('fixed-sidebar');
-            $('#thumbs-wrapper > div').removeClass('large-offset-4');
-            StickySidebar.changeTopBarBackgroundColor();
-        }
-    }
-});
-*/
 
 $(document).ready(function() {
 
@@ -21181,7 +21170,7 @@ $(document).ready(function() {
 
     // if sidebar is set up - listen for scroll events
     $(document).on('scroll', function() {
-        if (StickySidebar.isSetUp == true) {
+        if (StickySidebar.isSetUp) {
             StickySidebar.setUpTopBar();
             StickySidebar.setStickySidebarColumnHeight();
             StickySidebar.stickySidebar();
